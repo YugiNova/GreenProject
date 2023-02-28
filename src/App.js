a27daa12299fa76c160dedc38d84e96501654266
Yugi Nova <yuginovaniac@gmail.com>
2023-02-28 21:39:00 +0700

    Bai 2 - Add Item

--- src/App.js
+++ src/App.js
@@ -1,77 +1,11 @@
-import {useState} from "react";
+import React from "react";
+
+import Bai2 from "./Components/Bai2"
 
 const App = () => {
-  const [user,setUser] = useState({name:'A', email:'a@gmail.com'})
-  const [arr, setArr] = useState([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
-  const [count, setCount] = useState(0)
-  const [userArr,setUserArr] = useState([{name:'A', email:'a@gmail.com'},{name:'B', email:'b@gmail.com'},{name:'C', email:'c@gmail.com'}])
-
-  const onClick = () => {
-    setCount(count + 1)
-    const newUser = {
-      ...user,
-      email: `${user.name}${count}@gmail.com`
-    }
-    setUser(newUser);
-  }
-
-  const onClick2 = () =>{
-    const newArr = arr.map(item => {
-      return item*2
-    })
-    setArr(newArr)
-  }
-
-  const onClick3 = () =>{
-    const newArr = arr.filter(item => {
-      return undefined
-    })
-    setArr(newArr)
-  }
-  const onClick4 = () => {
-    const newArr = userArr.map(user => {
-      if(user.name === 'A'){
-        return {name:'D', email: 'd@gmail.com'}
-      }
-    })
-    setUserArr(newArr)
-  }
-
-   const onClick5 = () =>{
-    // const newUsers = userArr.push({name:'D', email: 'd@gmail.com'})
-    // setUserArr(newUsers)
-    setUserArr([
-      
-      {name:'D', email:'d@gmail.com'},
-      ...userArr
-    ])
-   }
-
-  return (
-    <div> 
-      {
-        arr.map((item)=> {
-          return(
-            <div>{item}</div>
-          )
-        })
-      }
-      {
-        userArr.map(user => {
-          return(
-            <div>
-              <div>Tên là: {user.name}</div>
-              <div>Email là: {user.email}</div>
-            </div>
-          )
-        })
-      }
-      <div>Count là: {count}</div>
-      <div>Tên là: {user.name}</div>
-      <div>Email là: {user.email}</div>
-      <button onClick ={onClick5}>Click me</button>
-    </div>
-  );
+    return ( 
+      < Bai2 / >
+    );
 }
 
 export default App;
---
+++ src/Components/Bai2.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { useState } from "react";
+
+const Bai2 = () => {
+    const [user,setUser] = useState({name: '', email: ''})
+    const [userList, setUserList] = useState([{name: 'A', email: 'a@gmail.com'},{name: 'B', email: 'b@gmail.com'}])
+    
+    const myStyle = {
+        border: "1px solid black"
+    }
+
+    const onChange = (e) =>{
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const onClick = () => {
+        setUserList([
+            ...userList,
+            user
+        ])
+        setUser({name:'', email:''})
+    }
+    
+    return(
+        <td>
+            <input name='name' value={user.name} onChange={onChange}/>
+            <input name='email' value={user.email} onChange={onChange}/>
+            <button onClick={onClick}>Add</button>
+
+            <table>
+                <tr>
+                    <th style={myStyle}>Họ và tên</th>
+                    <th style={myStyle}>Email</th>
+                </tr>
+                {
+                userList.map(user => {
+                    return(
+                        <tr>
+                            <td style={myStyle}>{user.name}</td>
+                            <td style={myStyle}>{user.email}</td>
+                        </tr>
+                    )
+                })
+            }
+            </table> 
+        </td>
+    )
+}
+
+export default Bai2
